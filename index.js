3c15818f0b6d9864bafebe43dd8fc682e90c6c08
Vignesh1625 <eligetivignesh@gmail.com>
2023-12-30 12:47:04 +0530

    delete workflows

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PORT = 3000;
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'mainPage.html'));
 });
 
 app.listen(PORT, () => {
